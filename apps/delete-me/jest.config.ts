a72754261369a267fc9014960e16780106f02d63
JF-PS <jfps.dev21@gmail.com>
2022-05-16 10:09:59 +0200

    initial commit

---
+++ apps/delete-me/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'delete-me',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/delete-me',
+};
